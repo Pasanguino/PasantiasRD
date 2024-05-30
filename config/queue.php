ac665bdaa2640dd20655766cca9bdbc1109ce94a
Jean Marte <jeanmarte22@gmail.com>
2024-05-30 18:41:38 -0400

    a
    a

--- config/queue.php
+++ config/queue.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_CONNECTION', 'databasse'),
+    'default' => env('QUEUE_CONNECTION', 'database'),
 
     /*
     |--------------------------------------------------------------------------
