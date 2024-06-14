dd85a7b128507ff6eb0cf21800608c085be33a04
stiven de la rosa brito <codestiven@gmail.com>
2024-06-14 12:14:28 -0400

    logo como icono

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+
 
         <!-- Scripts -->
         @routes
