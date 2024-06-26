0f9feeafa128140b71bd2e46cefbe1c74c7416b0
Jean Marte <jeanmarte22@gmail.com>
2024-06-26 09:25:48 -0400

    Update Migration - User_Profile migration update  0.7

--- database/migrations/2024_06_18_200213_create_user_profiles_table.php
+++ database/migrations/2024_06_18_200213_create_user_profiles_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('user_profiles', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('description');
+            $table->int('age');
+            $table->string('photo_path');
+            $table->string('identification_path');
+            $table->string('cv_path');
+            $table->foreignId('profession_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('type_user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('province_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
