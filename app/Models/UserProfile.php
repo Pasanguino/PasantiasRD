671a09a06a1513899370a4e49a5eae40c7dd9efb
Jean Marte <jeanmarte22@gmail.com>
2024-06-26 11:24:59 -0400

    Update Models- UserProfile Models update  0.20

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class UserProfile extends Model
 {
     use HasFactory;
+
+    
+public function profession(){
+    return $this->hasOne(Profession::class);
 }
+public function users(){
+    return $this->belongsTo(User::class);
+}
+}
+
