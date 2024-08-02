67cf07a60c8617503715d11b821c029f0812710a
AlbieriGarcia <albieri404@gmail.com>
2024-08-01 20:51:07 -0400

    insertando nuevos datos a la Base de datos

--- database/migrations/2024_06_18_200216_create_areas_table.php
+++ database/migrations/2024_06_18_200216_create_areas_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -16,6 +17,28 @@ return new class extends Migration
             $table->string('area_name');
             $table->timestamps();
         });
+
+        // Insertar datos en la tabla `areas`
+        DB::table('areas')->insert([
+            ['area_name' => 'Tecnología de la Información (TI)', 'created_at' => now(), 'updated_at' => now()],
+            ['area_name' => 'Marketing', 'created_at' => now(), 'updated_at' => now()],
+            ['area_name' => 'Medicina', 'created_at' => now(), 'updated_at' => now()],
+            ['area_name' => 'Diseño', 'created_at' => now(), 'updated_at' => now()],
+            ['area_name' => 'Recursos Humanos', 'created_at' => now(), 'updated_at' => now()],
+            ['area_name' => 'Contabilidad', 'created_at' => now(), 'updated_at' => now()],
+            ['area_name' => 'Ventas', 'created_at' => now(), 'updated_at' => now()],
+            ['area_name' => 'Administración', 'created_at' => now(), 'updated_at' => now()],
+            ['area_name' => 'Legal', 'created_at' => now(), 'updated_at' => now()],
+            ['area_name' => 'Operaciones', 'created_at' => now(), 'updated_at' => now()],
+            ['area_name' => 'Investigación y Desarrollo', 'created_at' => now(), 'updated_at' => now()],
+            ['area_name' => 'Atención al Cliente', 'created_at' => now(), 'updated_at' => now()],
+            ['area_name' => 'Producción', 'created_at' => now(), 'updated_at' => now()],
+            ['area_name' => 'Logística', 'created_at' => now(), 'updated_at' => now()],
+            ['area_name' => 'Compras', 'created_at' => now(), 'updated_at' => now()],
+            ['area_name' => 'Ingeniería', 'created_at' => now(), 'updated_at' => now()],
+            ['area_name' => 'Comunicación', 'created_at' => now(), 'updated_at' => now()],
+            ['area_name' => 'Seguridad', 'created_at' => now(), 'updated_at' => now()],
+        ]);
     }
 
     /**
