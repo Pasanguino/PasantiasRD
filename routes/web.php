1161ca51a363937be62dad4682016729bf8fa8d0
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-08-05 12:37:10 -0400

    poder crear companies

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AreaController;
 use App\Http\Controllers\VacancyController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\CompanyController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
@@ -32,6 +33,7 @@ Route::get('/vacancies_province/{province_id}', [ProvinceController::class, 'sho
 
 // Datos de provincias
 Route::get('/province-data', [ProvinceController::class, 'getProvinceData']);
+Route::get('/province', [ProvinceController::class,'index']);
 //http://pasantiasrd.test/t/search/buscar?buscar=Software
 Route::get('/vacante/search/buscar', [VacancyController::class, 'searchVacancy']) ->name('search');
 Route::get('/vacante/{id}', [VacancyController::class, 'show']);
@@ -86,6 +88,9 @@ Route::middleware(['empresa', 'auth'])->group(function () {
         return Inertia::render('VacanciesCompany');
     })->name('empresa');
 });
+
+Route::get('/companies', [CompanyController::class, 'index']);
+Route::post('/companies', [CompanyController::class, 'store']);
 
 // Rutas para autenticación de perfil
 Route::middleware('auth')->group(function () {
