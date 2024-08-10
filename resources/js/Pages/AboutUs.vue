81030cc38608e0ae2e0b62d394254f65a955ed40
jhoan <jhoanguzman428@gmail.com>
2024-08-09 20:09:42 -0400

    Imagen agregadas

--- resources/js/Pages/AboutUs.vue
+++ resources/js/Pages/AboutUs.vue
@@ -17,7 +17,7 @@ export default {
       leftColumnText: `<strong> <p class="text-lg ">Somos una plataforma dedicada a mejorar y facilitar la búsqueda y aplicación a pasantías en toda la República Dominicana.</strong><br>
                        Nuestro objetivo es conectar a estudiantes y profesionales con oportunidades de pasantías en empresas de diversos sectores, brindando una experiencia sencilla y efectiva. Estamos encargados de que la búsqueda de pasantias no sea tan complicada para los estudiantes. Con esta página pueden conectar con las empresas que solicita sus talentos como pasantes en el área de nuestra elección. De esta manera aumentamos la facilidad de que las empresas tengan buenos postulantes a las vacantes necesarias y facilitará al estudiante tener más oportunidades laborales, <br>
                        <b>Pasanguino</b>es una plataforma intuitiva con distintas formas de tener acceso a las vacantes disponibles. <b>La búsqueda de Pasantia no es tan mala con Pasanguino.</b></p>
-                       <p class="text-lg mt-4">Para más información, no dudes <a href="#" class="text-red-300 hover:underline">contactarnos</a>.</p>`
+                       <p class="text-lg mt-4">Para más información, no dudes <a href="#" class="text-red-300 hover:underline">contactanos</a>.</p>`
 
                        
      }
